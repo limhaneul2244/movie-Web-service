10fe4936a7a682d635c3cdbddb734958fed0fa65
임하늘 <hnlim2244@gmail.com>
2023-02-05 22:11:21 +0900

    #7.2강 공부

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Coin from './Coin';
 import ToDoList from './ToDoList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     {/* <App /> */}
-    <ToDoList/>
+    {/* <ToDoList/> */}
+    <Coin/>
   </>
 );
 
