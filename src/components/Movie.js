941beb872a38c352b471cc248bbd9a9d1388cc03
limhaneul2244 <hnlim2244@gmail.com>
2023-02-06 18:40:29 +0900

    수정

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,7 +9,7 @@ const Movie = ({id, largeCoverImage, title, summary, genres}) => {
             <h3><Link to={`/Detail/${id}`}>{title}</Link></h3>
             {summary !== "" ? <div className='textElip'>{summary}</div> : <div>no data</div>}
             <ul>
-                {genres.map((item, id) => <li key={id}>{item}</li>)}
+                {genres.map((item, id) => <li key={id}>{item}</li>)} 
             </ul>
         </div>
     );
